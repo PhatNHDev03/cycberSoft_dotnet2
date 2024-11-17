13cee55b2682b0128e5a2c747d607c24169f1f14
fat872003 <factnotphat0807@gmail.com>
2024-11-17 18:42:07 +0700

    new

--- session2/obj/Debug/net8.0/session2.AssemblyInfo.cs
+++ session2/obj/Debug/net8.0/session2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("session2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec9e679fabb1ea97eb165e7c8f6c8134bec2383f")]
 [assembly: System.Reflection.AssemblyProductAttribute("session2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("session2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
