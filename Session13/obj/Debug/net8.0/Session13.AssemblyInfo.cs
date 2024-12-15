2452b1a8f982846362e3fda2ea73086c8a636344
fat872003 <factnotphat0807@gmail.com>
2024-12-15 13:41:53 +0700

    html-css

--- Session13/obj/Debug/net8.0/Session13.AssemblyInfo.cs
+++ Session13/obj/Debug/net8.0/Session13.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Session13")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac63d3b3aeb95677f8c2f6bc8a65bb34d2322d17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5193ee523191c2d76c96b8a25746669d7e677e73")]
 [assembly: System.Reflection.AssemblyProductAttribute("Session13")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Session13")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
