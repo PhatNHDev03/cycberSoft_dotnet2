8560cba9156491d07f8aa3d69018ba31241e67ea
fat872003 <factnotphat0807@gmail.com>
2025-01-24 23:32:35 +0700

    on lai 20

--- html_css/Session20_23_tailWind_css/tailwind.config.js
+++ html_css/Session20_23_tailWind_css/tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    // custom ở chỗ extend này nè
+    extend: {
+      container: {
+        center: true
+      },
+      backgroundColor: {
+        mainColor: "#A2D2DF",
+        secondColor: "#F6EACB",
+        bodyColor: "#151937"
+      }, textColor: {
+        mainColor: "F6EACB",
+        profileColor: "#9334e9"
+      }, borderColor: {
+        profileColor: "#9334e9"
+      }, backgroundImage: {
+        'gradient-text': "linear-gradient(to left, #FFCBA0, #DD5789, #9B5BE6);"
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
 
