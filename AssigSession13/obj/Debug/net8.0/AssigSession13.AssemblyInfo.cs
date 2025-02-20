b36c43cf6da15bd7e54d0804e06749945b1d7180
PhatNguyenDev <139215308+PhatNHDev03@users.noreply.github.com>
2025-02-20 15:52:58 +0700

    finish assign 24

--- AssigSession13/obj/Debug/net8.0/AssigSession13.AssemblyInfo.cs
+++ AssigSession13/obj/Debug/net8.0/AssigSession13.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssigSession13")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f9bfd089f0d3e7cf3473660fea9f7e7e99c40b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76af3708abc8f5e2b5efc7074b547fee935ff570")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssigSession13")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssigSession13")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ sesion24/Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using sesion24;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//dotnet new blazorwasm -o sesion24
+await builder.Build().RunAsync();
